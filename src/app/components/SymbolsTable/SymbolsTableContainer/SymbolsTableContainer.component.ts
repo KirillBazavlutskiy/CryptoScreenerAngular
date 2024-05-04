55cb1210283c7ed9c0e50e324c6921dd3e5edeb9
Kirill <bazik345@gmail.com>
2024-05-04 14:29:57 +0300

    improved perfomance

--- src/app/components/SymbolsTable/SymbolsTableContainer/SymbolsTableContainer.component.ts
+++ src/app/components/SymbolsTable/SymbolsTableContainer/SymbolsTableContainer.component.ts
@@ -1,11 +1,13 @@
 import {Component} from "@angular/core";
-import {SolidityModel} from "../../../models/RestApi/SolidityFinderApi/GetSolidity.model";
+import {LimitType, SolidityModel} from "../../../models/RestApi/SolidityFinderApi/GetSolidity.model";
 import {SymbolsTableItemComponent} from "../SymbolsTableItem/SymbolsTableItem.component";
 import {AsyncPipe, NgForOf} from "@angular/common";
 import {SymbolsListItemComponent} from "../../SymbolsList/SymbolsListItem/symbolsListItem.component";
 import {Store} from "@ngrx/store";
 import {Observable} from "rxjs";
 import {StoreModel} from "../../../store/Store.model";
+import {StylingOptionsModel} from "../../../models/Options/StylingOptions.model";
+import {getLevelColor} from "../../../services/Styling/GetColorStyle";
 
 @Component({
   selector: 'app-symbols-table-container',
@@ -21,8 +23,25 @@ import {StoreModel} from "../../../store/Store.model";
 
 export class SymbolsTableContainerComponent {
   symbolsList$: Observable<SolidityModel[]>;
+  stylingOptions$: Observable<StylingOptionsModel>;
+
+  getSolidityDistanceColor: (solidityType: LimitType, value: number) => string;
+  getSolidityRatioColor: (value: number) => string;
 
   constructor(private store: Store<StoreModel>) {
     this.symbolsList$ = store.select('symbolsList');
+
+    this.getSolidityDistanceColor = (solidityType: LimitType, value: number) => "";
+    this.getSolidityRatioColor = (value: number) => "";
+
+    this.stylingOptions$ = this.store.select(store => store.stylingOptions);
+
+    this.stylingOptions$.subscribe(newStylingOptions => {
+      this.getSolidityDistanceColor = (solidityType: LimitType, value: number) => {
+        if (solidityType === "asks") return getLevelColor(value, newStylingOptions.upToPrice.asks)
+        else return getLevelColor(value, newStylingOptions.upToPrice.bids)
+      }
+      this.getSolidityRatioColor = (value: number) => getLevelColor(value, newStylingOptions.solidityRatio)
+    })
   }
 }
